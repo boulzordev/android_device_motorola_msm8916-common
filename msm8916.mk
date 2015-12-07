c4cb5b22759cc00d37efc78799b62c4880ff9618
Alberto97 <albertop2197@gmail.com>
2015-12-07 13:53:19 +0100

    msm8916-common: Move system/etc/data to vendor

--- msm8916.mk
+++ msm8916.mk
@@ -72,9 +72,6 @@ PRODUCT_PACKAGES += \
 
 # Configs
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
-    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
-    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
     $(LOCAL_PATH)/configs/data/apns-conf.xml:system/etc/apns-conf.xml
 
 # Display
