cd24c0da4ee538da200d719798d18f1c73436b77
Alberto97 <albertop2197@gmail.com>
2016-02-17 16:31:41 +0100

    msm8916-common: PRODUCT_AAPT_PREF_CONFIG is device specific
    * Only merlin and osprey are actually xhdpi devices
    
    Change-Id: I8100f64aef724297830a3601ea32f5865eef6d9e

--- msm8916.mk
+++ msm8916.mk
@@ -45,7 +45,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
